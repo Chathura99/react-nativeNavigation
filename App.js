d0ebf139d39dbfdf3a9a0e6398a16f695aa51555
Chathura <chathura.madura11@gmail.com>
2022-06-19 22:49:14 +0530

    stack navigation with comp A,B,C

--- App.js
+++ App.js
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const PageA = () => {
+  // without using props navigation
+  // const navigation =useNavigation();
+  return (
+    <View style={styles.container}>
+      <Text>I am A</Text>
+      <Button
+        title="Go to Home Page"
+        onPress={() => navigation.navigate('HomePage')}
+      ></Button>
+    </View>
+  );
+};
+
+const PageC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>I am C</Text>
+      <Button
+        title="Go to Home Page"
+        onPress={() => navigation.navigate('HomePage')}
+      ></Button>
+    </View>
+  );
+};
+
+const PageB = () => {
+  return (
+    <View style={styles.container}>
+      <Text>I am B</Text>
+      <Button
+        title="Go to Home Page"
+        onPress={() => navigation.navigate('HomePage')}
+      ></Button>
+    </View>
+  );
+};
+
+const HomePage = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>I am Home Page</Text>
+      <Button
+        title="Go to A"
+        onPress={() => navigation.navigate('PageA')}
+      ></Button>
+            <Button
+        title="Go to B"
+        onPress={() => navigation.navigate('PageB')}
+      ></Button>
+            <Button
+        title="Go to B"
+        onPress={() => navigation.navigate('PageC')}
+      ></Button>
+    </View>
+  );
+};
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    //Stack Navigator
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="HomePage" component={HomePage} />
+        <Stack.Screen name="PageA" component={PageA} />
+        <Stack.Screen name="PageB" component={PageB} />
+        <Stack.Screen name="PageC" component={PageC} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-evenly",
   },
 });
